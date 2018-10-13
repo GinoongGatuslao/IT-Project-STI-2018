3d3e1fdb812a64c883590a64eb0421891b4a3418
patledesma <patdledesma@gmail.com>
2018-10-13 19:45:33 +0800

    removed hardcoded codes

--- mb40mktg-c#/WindowsFormsApp1/Login.cs
+++ mb40mktg-c#/WindowsFormsApp1/Login.cs
@@ -7,8 +7,8 @@ namespace WindowsFormsApp1
 {
     public partial class Login : Form
     {
-        public static string api_token = "2tZegRSDH1is1I2Kxz19D21AJAf8kRn4Gbf1vtQNQP5OnObPn8NIHmKMIgEu";
-        public static int id = 4;
+        public static string api_token = string.Empty;
+        public static int id = 0;
         public static int user_type = 0;
 
         public Login()
--- mb40mktg-c#/WindowsFormsApp1/Settings.cs
+++ mb40mktg-c#/WindowsFormsApp1/Settings.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp1
 {
     public partial class Settings : Form
     {
-        public static string baseUrl = "https://a1ea2ff2.ngrok.io";
+        public static string baseUrl = string.Empty;
 
         public Settings()
         {
